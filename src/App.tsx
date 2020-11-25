21c6eea9d54d02e315be807f488e5182a58c8d1c
Yazeed Bzadough <ybzadough@gmail.com>
2020-11-25 17:15:10 -0500

    demo usable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import gsap from 'gsap';
 import './App.css';
 import { TwoPointers } from './arrays/findTheDuplicate/TwoPointers';
@@ -6,48 +6,98 @@ import { TwoPointers } from './arrays/findTheDuplicate/TwoPointers';
 const input = [3, 7, 5, 1, 9, 5, 6, 2, 4, 8];
 const sorted = [...input].sort((a, z) => a - z);
 
+export type FinalAnswer = string;
+
+type State = {
+  scene: Scene;
+  finalAnswer: FinalAnswer;
+};
+
+type Scene = 'first' | 'second' | 'third' | 'fourth';
+
+type Action =
+  | { type: 'PLAY_TWO_POINTERS' }
+  | { type: 'SCENE_END'; finalAnswer: FinalAnswer };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'PLAY_TWO_POINTERS':
+      return {
+        ...state,
+        scene: 'third'
+      };
+
+    case 'SCENE_END':
+      return {
+        ...state,
+        scene: 'fourth',
+        finalAnswer: action.finalAnswer
+      };
+  }
+};
+
+const initialState: State = {
+  scene: 'first',
+  finalAnswer: ''
+};
+
+const tl = gsap.timeline({ delay: 0.5 });
+
 const App = () => {
-  const [showLast, setShowLast] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    const tl = gsap.timeline({ delay: 0.5 });
-
-    tl.set('.first', { y: -100, display: 'block' });
-    tl.to('.first', { y: 0, duration: 2, ease: 'elastic' });
-
-    tl.to(
-      '.second',
-      {
-        display: 'block',
-        y: 20,
-        duration: 2,
-        ease: 'elastic'
-      },
-      '+=2'
-    ).then(() => setShowLast(true));
-
-    tl.to(
-      '.third',
-      {
-        display: 'block',
-        y: 40,
-        duration: 2,
-        ease: 'elastic'
-      },
-      '+=2'
-    );
+    tl.from('.first', { xPercent: -100 })
+      .from('.second', { xPercent: -100 }, '+=1')
+      .from('.element', { opacity: 0 }, '+=1')
+      .from(
+        '.third',
+        {
+          xPercent: -100,
+          onStart: () => dispatch({ type: 'PLAY_TWO_POINTERS' })
+        },
+        '+=1'
+      )
+      .addPause()
+      .from('.four', { xPercent: -100 }, '+=1');
   }, []);
+
+  useEffect(() => {
+    if (state.scene !== 'fourth') {
+      return;
+    }
+
+    tl.play();
+  }, [state.scene]);
 
   return (
     <main>
-      <div className="first hide">
+      <div className="first">
         <h4>Receive input</h4>[{input.join(', ')}]
       </div>
-      <div className="second hide">
-        <h4>Sort the array</h4>[{sorted.join(', ')}]
+
+      <div className="second">
+        <h4>Sort the array</h4>[
+        {sorted.map((v, index) => (
+          <span className="element">
+            {v} {index !== sorted.length - 1 && ', '}
+          </span>
+        ))}
+        ]
       </div>
+
       <div className="third">
-        {showLast && <TwoPointers sortedArray={sorted} />}
+        <TwoPointers
+          sortedArray={sorted}
+          start={state.scene === 'third'}
+          onComplete={(finalAnswer) =>
+            dispatch({ type: 'SCENE_END', finalAnswer })
+          }
+        />
+      </div>
+
+      <div className="four">
+        <h4>The duplicate number is {state.finalAnswer}</h4>
       </div>
     </main>
   );
--- src/arrays/findTheDuplicate/TwoPointers.tsx
+++ src/arrays/findTheDuplicate/TwoPointers.tsx
@@ -1,23 +1,35 @@
 import gsap from 'gsap';
 import React, { FC, useEffect, useState } from 'react';
+import { FinalAnswer } from '../../App';
 import './TwoPointers.scss';
 
 interface TwoPointersProps {
-  sortedArray: any[];
+  sortedArray: number[];
+  start: boolean;
+  onComplete: (answer: FinalAnswer) => void;
 }
 
 const tl = gsap.timeline({ repeat: -1, yoyo: true, repeatDelay: 0 });
 
-export const TwoPointers: FC<TwoPointersProps> = ({ sortedArray }) => {
+export const TwoPointers: FC<TwoPointersProps> = ({
+  sortedArray,
+  start,
+  onComplete
+}) => {
   const [{ p1, p2 }, setValues] = useState({ p1: 0, p2: 1 });
+  const sequenceDone = p2 === sortedArray.length - 1;
+  const answerFound = sortedArray[p1] === sortedArray[p2];
 
   useEffect(() => {
-    if (p2 === sortedArray.length - 1) {
+    if (!start || sequenceDone) {
       return;
     }
 
-    if (sortedArray[p1] === sortedArray[p2]) {
-      tl.fromTo('.pointer', { y: '-=5' }, { y: '+=5' });
+    if (answerFound) {
+      tl.fromTo('.pointer.one', { y: '+=10' }, { y: '-=20' });
+      tl.fromTo('.pointer.two', { y: '-=10' }, { y: '+=20' }, 0);
+
+      onComplete(sortedArray[p1].toString());
 
       return;
     }
@@ -28,7 +40,7 @@ export const TwoPointers: FC<TwoPointersProps> = ({ sortedArray }) => {
         p2: p2 + 1
       }));
     }, 1000);
-  }, [p1, p2]);
+  }, [p1, p2, start]);
 
   return (
     <div className="two-pointers">
@@ -38,34 +50,40 @@ export const TwoPointers: FC<TwoPointersProps> = ({ sortedArray }) => {
         [
         {sortedArray.map((v, index) => {
           const isLast = index === sortedArray.length - 1;
+          const isP1 = index === p1;
+          const isP2 = index === p2;
+
           const element = (
-            <span className="element">
+            <span
+              className={classNames([
+                ['element', true],
+                ['answer', isP1 || isP2]
+              ])}
+            >
               {v} {isLast ? '' : ', '}
             </span>
           );
 
-          if (p1 === index) {
-            return (
-              <>
-                <span className="pointer one" />
-                {element}
-              </>
-            );
-          }
-
-          if (p2 === index) {
-            return (
-              <>
-                <span className="pointer two" />
-                {element}
-              </>
-            );
-          }
-
-          return element;
+          return (
+            <>
+              <span
+                className={classNames([
+                  ['pointer', isP1 || isP2],
+                  ['one', isP1],
+                  ['two', isP2]
+                ])}
+              ></span>
+              {element}
+            </>
+          );
         })}
         ]
       </div>
     </div>
   );
 };
+
+type ClassNameList = [string, boolean][];
+
+const classNames = (p: ClassNameList) =>
+  p.reduce((result, [cn, cond]) => (cond ? `${result} ${cn}` : result), '');
