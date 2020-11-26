daa2ad96083e13a3c88c73607697e9bc7a5684a4
Yazeed Bzadough <ybzadough@gmail.com>
2020-11-26 12:27:14 -0500

    refactor algo

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,11 @@
-import React, { useEffect } from 'react';
-import gsap from 'gsap';
+import React from 'react';
 import './App.css';
-import {
-  findDuplicate,
-  TwoPointers
-} from './arrays/findTheDuplicate/TwoPointers';
-import { input, sorted } from './state';
-
-const tl = gsap.timeline();
+import { TwoPointers } from './arrays/findTheDuplicate/TwoPointers';
 
 const App = () => {
-  const answer = findDuplicate(sorted);
-  const answerIndex = sorted.findIndex((v) => v === answer);
-
-  useEffect(() => {
-    const arrayElements = Array.from(document.querySelectorAll('.element'));
-
-    tl.from('.first', { xPercent: -100 })
-      .from('.second', { xPercent: -100 }, '+=1')
-      .from('.element-second', { opacity: 0, stagger: { amount: 1.5 } }, '+=1')
-      .from('.third', { xPercent: -100 })
-      .to('.pointers', {
-        keyframes: arrayElements.slice(0, answerIndex).map((e) => {
-          const { width } = e.getBoundingClientRect();
-
-          return {
-            x: `+=${width}`,
-            duration: 0.1,
-            delay: 0.5
-          };
-        })
-      })
-      .from('.four', { xPercent: -100 })
-      .fromTo(
-        '.pointer.one',
-        { y: '+=10', repeat: -1, yoyo: true, repeatDelay: 0 },
-        { y: '-=20', repeat: -1, yoyo: true, repeatDelay: 0 }
-      )
-      .fromTo(
-        '.pointer.two',
-        { y: '-=10', repeat: -1, yoyo: true, repeatDelay: 0 },
-        { y: '+=20', repeat: -1, yoyo: true, repeatDelay: 0 },
-        '<'
-      );
-  }, []);
-
   return (
     <main>
-      <div className="first scene">
-        <h4>Receive input</h4>
-        <span>[{input.join(', ')}]</span>
-      </div>
-
-      <div className="second scene">
-        <h4>Sort the array</h4>[
-        {sorted.map((v, index) => (
-          <span className="element-second" key={index}>
-            {v} {index !== sorted.length - 1 && ', '}
-          </span>
-        ))}
-        ]
-      </div>
-
-      <div className="third scene">
-        <TwoPointers sortedArray={sorted} />
-      </div>
-
-      <div className="four scene">
-        <p>
-          {answer !== null
-            ? `Duplicate found: ${answer}`
-            : 'No duplicate found in array'}
-        </p>
-      </div>
+      <TwoPointers array={[8, 4, 10, 2, 1, 7, 3, 5, 8]} />
     </main>
   );
 };
--- src/arrays/findTheDuplicate/TwoPointers.tsx
+++ src/arrays/findTheDuplicate/TwoPointers.tsx
@@ -1,34 +1,96 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import gsap from 'gsap';
 import './TwoPointers.scss';
 
+const tl = gsap.timeline();
+
 interface TwoPointersProps {
-  sortedArray: number[];
+  array: number[];
 }
 
-export const TwoPointers: FC<TwoPointersProps> = ({ sortedArray }) => {
-  return (
-    <div className="two-pointers">
-      <h4>March two pointers until they're equal</h4>
+export const TwoPointers: FC<TwoPointersProps> = ({ array }) => {
+  const sorted = [...array].sort(ascending);
+  const answer = findDuplicate(sorted);
+  const answerIndex = sorted.findIndex((v) => v === answer);
 
-      <div className="pointers">
-        <span className="pointer one"></span>
-        <span className="pointer two"></span>
+  useEffect(() => {
+    const arrayElements = Array.from(document.querySelectorAll('.element'));
+
+    tl.from('.first', { xPercent: -100 })
+      .from('.second', { xPercent: -100 }, '+=1')
+      .from('.element-second', { opacity: 0, stagger: { amount: 1.5 } }, '+=1')
+      .from('.third', { xPercent: -100 })
+      .to('.pointer', {
+        keyframes: arrayElements.slice(0, answerIndex).map((e) => {
+          const { width } = e.getBoundingClientRect();
+
+          return {
+            x: `+=${width}`,
+            duration: 0.1,
+            delay: 0.5
+          };
+        }),
+        onComplete: () => {
+          gsap.fromTo(
+            '.pointer.one, .pointer.two',
+            { y: (i) => (i % 2 ? '+=10' : '-=10') },
+            { y: (i) => (i % 2 ? '-=20' : '+=20'), repeat: -1, yoyo: true }
+          );
+        }
+      })
+      .from('.four', { xPercent: -100 });
+  }, []);
+
+  return (
+    <div>
+      <div className="first scene">
+        <h4>Receive input</h4>
+        <span>[{array.join(', ')}]</span>
       </div>
 
-      <div className="container">
-        [
-        {sortedArray.map((v, index) => {
-          return (
-            <span className="element" key={index}>
-              {v},{' '}
-            </span>
-          );
-        })}
+      <div className="second scene">
+        <h4>Sort the array</h4>[
+        {sorted.map((v, index) => (
+          <span className="element-second" key={index}>
+            {index < sorted.length - 1 ? `${v}, ` : v}
+          </span>
+        ))}
         ]
+      </div>
+
+      <div className="third scene">
+        <div className="two-pointers">
+          <h4>March two pointers until they're equal</h4>
+
+          <span className="pointer one" />
+          <span className="pointer two" />
+
+          <div className="container">
+            [
+            {sorted.map((v, index) => {
+              return (
+                <span className="element" key={index}>
+                  {index < sorted.length - 1 ? `${v}, ` : v}
+                </span>
+              );
+            })}
+            ]
+          </div>
+        </div>
+      </div>
+
+      <div className="four scene">
+        <p>
+          {answer !== null
+            ? `Duplicate found: ${answer}`
+            : 'No duplicate found in array'}
+        </p>
       </div>
     </div>
   );
 };
+
+const ascending = (a: number, b: number) => (a < b ? -1 : a > b ? 1 : 0);
 
 export const findDuplicate = (arr: number[]) => {
   let p1 = 0;
